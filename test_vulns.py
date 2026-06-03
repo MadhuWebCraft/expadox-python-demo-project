ec5fa9a6ad319bbb5774d3ce86e692cf172c4e02
Challa.Madhumitha <madhumitha.challa05@gmail.com>
2026-06-04 00:24:12 +0530

    Add test vulnerabilities for ShieldFlow demo

---
+++ test_vulns.py
@@ -0,0 +1,18 @@
+# Fake AWS Secret (for Gitleaks)
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Hardcoded password (Semgrep)
+PASSWORD = "admin123"
+
+# SQL Injection example (Semgrep)
+def login(username):
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    return query
+
+# Insecure command execution (Semgrep)
+import os
+
+def run_command(user_input):
+    os.system(user_input)
+  
